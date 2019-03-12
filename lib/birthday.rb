1df47d4365b9c82770ced6498e40e2791f58b0e0
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-12 22:20:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
+    binding.pry
     if age =< 12
       print "Happy Birthday #{kids_name}! You are now #{age} years old! "
     else
